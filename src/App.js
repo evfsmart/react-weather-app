7406c7bd9901f5f391bc003b038c7857fa3e0d61
evfsmart <evfsmart@gmail.com>
2021-06-10 11:26:17 +0100

    Set up basic JSX structure inside Weather.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project was coded by Emma Smart and is{" "}
           <a

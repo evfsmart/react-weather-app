fd3fff703bfa46c41a94cd6d4bf37cb77f7aa9dd
evfsmart <evfsmart@gmail.com>
2021-06-10 10:55:05 +0100

    Added the GitHub repo URL to Footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Emma Smart and is{" "}
+        <a
+          href="https://github.com/evfsmart/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }

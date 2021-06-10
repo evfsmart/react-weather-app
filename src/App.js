b09339b581050fb666062b457564f1471253305c
evfsmart <evfsmart@gmail.com>
2021-06-10 11:09:43 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Emma Smart and is{" "}
-        <a
-          href="https://github.com/evfsmart/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Emma Smart and is{" "}
+          <a
+            href="https://github.com/evfsmart/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

764402cdbd5731321daf1c3b58417a59bebea45a
evfsmart <evfsmart@gmail.com>
2021-06-11 15:50:40 +0100

    Changed defaultCity to London

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="Resolute" />
+        <Weather defaultCity="London" />
         <footer>
           Coded by{" "}
           <a
